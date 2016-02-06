38519cb838dfcfa5d5d2232332e3694d68c16f52
Duduś <Dudu@fOrest>
2016-02-06 15:20:24 +0100

    zapomniane

---
+++ WebContent/js/controllers/dupaCtrl.js
@@ -0,0 +1,3 @@
+app.controller('dupaCtrl', ['$scope', function ($scope) {
+
+}]);
---
+++ WebContent/js/mainModule.js
@@ -0,0 +1,4 @@
+/**
+ * Created by winio_000 on 2015-12-13.
+ */
+var app = angular.module('mainModule', ['ui.bootstrap']);
